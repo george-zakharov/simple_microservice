cafe69da1f10a9ff850e58f67ca90b2fddd49d1f
george_zakharov <george_zakharov@mail.ru>
2020-11-09 21:33:44 +0300

    Add about

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", helloServer)
+	http.HandleFunc("/", requestHandler)
+	http.HandleFunc("/about", about)
 	http.ListenAndServe(":8080", nil)
 }
 
-func helloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome, %s!", r.URL.Path[1:])
+}
+
+// Message type...
+type Message struct {
+	Text string
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	m := Message{"Welcome to Simple API"}
+	b, err := json.Marshal(m)
+
+	if err != nil {
+		panic(err)
+	}
+
+	w.Write(b)
 }
